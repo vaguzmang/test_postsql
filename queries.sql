527ff2685fd08c83a283eeca3d8bb0e4c3443de3
JoelSantiagoMP <yessirrr1105@gmail.com>
2026-07-06 20:19:39 -0500

    Refactor SQL queries for sales and customer analysis
    Updated SQL queries to improve accuracy and added new queries for product sales and customer inactivity.

--- queries.sql
+++ queries.sql
@@ -13,7 +13,33 @@ where v.fecha between '2026-07-01' and '2026-07-30';
 
 /* Obtener el cliente con más compras realizadas. */
 
-select c.id_cliente, c.nombre1, c.apellido1, c.telefono, count(v.id_cliente)
+select c.id_cliente, c.nombre1, c.apellido1, c.telefono, count(v.id_venta) as total_transacciones
 from cliente c
 join venta v on c.id_cliente = v.id_cliente
-order by v.id_cliente desc;
+group by c.id_cliente, c.nombre1, c.apellido1, c.telefono
+order by total_transacciones desc
+limit 1;
+
+/* Listar los productos más vendidos */
+
+select p.id_producto, p.nombre, p.precio, dv.cantidad
+from producto p
+join detalle_venta dv on p.id_producto = dv.id_producto
+order by dv.cantidad desc
+limit 1;
+
+/* Consultar ventas realizadas en un rango de fechas */
+
+select v.id_venta
+from venta v 
+where v.fecha between '2026-01-01' and '2026-06-01';
+
+/* Identificar clientes que no han comprado en los últimos 6 meses */
+
+select c.id_cliente, c.nombre1, c.apellido1, c.correo, c.telefono
+from cliente c 
+where not exists (select 1 from venta v
+	where v.id_cliente = c.id_cliente
+	and v.fecha >= current_timestamp - interval '6 months'
+);
+
